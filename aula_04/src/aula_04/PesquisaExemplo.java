bc71af09fc73d75a498933cddf6f10c621c1c215
Aylla Scaglia <aylla@scaglia.com.br>
2025-04-16 13:54:50 -0300

    :bug:fix: Correção de Warning

--- aula_04/src/aula_04/PesquisaExemplo.java
+++ aula_04/src/aula_04/PesquisaExemplo.java
@@ -2,62 +2,63 @@ package aula_04;
 
 import java.util.Scanner;
 
-public class Pesquisa {
+public class PesquisaExemplo {
 
 	public static void main(String[] args) {
 		
-        Scanner leia = new Scanner (System.in);
-		
-		int idade, esporte, futebol = 0, voleibolM18 = 0, basqueteMn18 = 0, totalRespostas = 0; 
+		Scanner leia = new Scanner(System.in);
+		int idade,
+			sporte,
+			futebol = 0,
+			voleibolM18 = 0,
+			basqueteMn18 = 0,
+			totalRespostas = 0;
 		long somaIdades = 0;
 		double media = 0;
 		String continua = "S";
 		
 		while (continua.equalsIgnoreCase("S")) {
-			
-		
+
 			System.out.println("\nInforme a sua idade: ");
 			idade = leia.nextInt();
-			
+
 			System.out.println("\nInforme o seu esporte favorito(1- Futebol / 2- Vôlei / 3- Basquete / 4- Outros): ");
-			esporte = leia.nextInt();
-			
+			sporte = leia.nextInt();
+
 			// Quantas pessoas gostam de futebom? //
-			if(esporte == 1) {
-				futebol ++;
+			if (sporte == 1) {
+				futebol++;
 			}
-			
+
 			// Quantas pessoas gostam de voleibol e são maiores de 18 anos //
-			if (esporte == 2 && idade > 18) {
-				voleibolM18 ++;
-	
+			if (sporte == 2 && idade > 18) {
+				voleibolM18++;
+
 			}
-			
+
 			// Quantas pessoas gostam de basquetebol e são menores de 18 anos //
-			if (esporte == 3 && idade < 18) {
-				basqueteMn18 ++;
-				
+			if (sporte == 3 && idade < 18) {
+				basqueteMn18++;
+
 			}
-			
-			totalRespostas ++;
-			
+
+			totalRespostas++;
+
 			somaIdades += idade;
-			
+
 			System.out.println("Deseja continuar (S/N)");
 			leia.skip("\\R");
 			continua = leia.nextLine().toUpperCase();
-			
+
 		}
+		
 		media = Math.round(somaIdades / totalRespostas);
 		System.out.printf("\nTotal de pessoas que gostam de futebol: %d", futebol);
 		System.out.printf("\nTotal de pessoas que gostam de voleibol e são maiores de idade: %d", voleibolM18);
 		System.out.printf("\nTotal de pessoas que gostam de basquetebol e são menores de idade: %d", basqueteMn18);
 		System.out.printf("\nMédia de idade: %.2f", media);
-		
-		
+
 		leia.close();
- 
-    }
+	}
 
 }
-
